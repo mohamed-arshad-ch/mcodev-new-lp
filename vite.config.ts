6b4d527298df79ae6ce4ccebfe81d90fa1722215
mohamed-arshad-ch <mohamedarshadcholasseri5050@gmail.com>
2025-02-24 09:14:22 +0530

    seo configurated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+import sitemap from "vite-plugin-sitemap";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    sitemap({
+      hostname: "https://mcodevbytes.in", // Replace with your actual domain
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
